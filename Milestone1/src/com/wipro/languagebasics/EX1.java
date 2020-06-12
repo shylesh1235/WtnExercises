b533dbf77ca98ff2be6e5e9f21e8ddfdbedd8a11
SHYLESH <SHYLESH@DESKTOP-NN19MM0>
2020-06-12 08:13:03 +0530

    this are my wtn problems

---
+++ Milestone1/src/com/wipro/languagebasics/EX1.java
@@ -0,0 +1,9 @@
+package com.wipro.languagebasics;
+
+public class EX1 {
+	
+public static void main(String[] args)
+{
+	System.out.println(args[0] + "Technologies" + args[1]);
+}
+}
---
+++ Milestone1/src/com/wipro/languagebasics/EX2.java
@@ -0,0 +1,11 @@
+package com.wipro.languagebasics;
+
+public class EX2 {
+    public static void main(String args[] )
+    {
+    	System.out.println("Welcome"+args[0]);
+    }
+    
+    
+    
+}
